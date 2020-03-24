2e738991e660f568bb4c887e492e2278a6769983
atomic1 <62310071@go.buu.ac.th>
2020-03-24 10:57:08 +0700

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310071"; ?><br>
+<?php echo "ชื่อ-สกุล นายพงศ์ภรณ์ จันทาพูน."; ?><br>
+<iframe width="500" height="500"
+src="https://www.youtube.com/watch?v=QIhUwoZ7iIQ">
+</iframe>
+</BODY>
+</HTML>
